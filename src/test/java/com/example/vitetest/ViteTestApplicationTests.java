d51b096ec3b5ac5ad90e958ca270c641254f56f1
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-07-10 13:41:30 +0200

    Initial commit

---
+++ src/test/java/com/example/vitetest/ViteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
